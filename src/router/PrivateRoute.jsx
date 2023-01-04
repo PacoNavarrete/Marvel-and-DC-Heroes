09c806b6ac438e6d12454510c080daf9212e8bc4
PacoNavarrete <102440729+PacoNavarrete@users.noreply.github.com>
2023-01-03 21:59:49 -0800

    public and private routes finished

---
+++ src/router/PrivateRoute.jsx
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../auth/context/AuthContext';
+
+export const PrivateRoute = ({ children }) => {
+  const { authState } = useContext(AuthContext);
+
+  return authState.logged ? children : <Navigate to="/login" />;
+};
